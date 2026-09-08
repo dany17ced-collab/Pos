aa10ddc3cd5b462fe688f729f31529c163dce3af
dany17ced-collab <dany17ced@gmail.com>
2026-09-08 14:05:36 -0500

    Update compileSdk and targetSdk to version 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.tuempresa.possystem"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.tuempresa.possystem"
         minSdk = 26 // Android 8.0 - necesario para Bluetooth SPP estable y CameraX
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0.0"
 
