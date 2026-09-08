c3e38e9f8b0b561a59460c9ff49bd43198d8764f
dany17ced-collab <dany17ced@gmail.com>
2026-09-08 14:41:43 -0500

    Update build.gradle.kts for Compose integration
    Added Compose plugin and removed composeOptions.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("com.google.devtools.ksp")
     id("org.jetbrains.kotlin.plugin.serialization")
+    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
@@ -68,9 +69,6 @@ android {
         buildConfig = true
     }
 
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.14"
-    }
 
     packaging {
         resources {
