ca01f77e00d8626108c5191028d8999c294c5993
dany17ced-collab <dany17ced@gmail.com>
2026-09-12 00:17:47 -0500

    Delete Application.kt

--- Application.kt
+++
@@ -1,9 +0,0 @@
-// En tu clase POSApplication.kt:
-
-class POSApplication : Application() {
-    // ... tu código existente
-    
-    val usuarioRepository: UsuarioRepository by lazy {
-        UsuarioRepository(database.usuarioDao())
-    }
-}
