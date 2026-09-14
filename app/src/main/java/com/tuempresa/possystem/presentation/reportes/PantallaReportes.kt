c6ce6c3a7ac74e1cd06618f8359901707292c751
dany17ced-collab <dany17ced@gmail.com>
2026-09-13 21:12:38 -0500

    Add imePadding to Surface in PantallaReportes

--- app/src/main/java/com/tuempresa/possystem/presentation/reportes/PantallaReportes.kt
+++ app/src/main/java/com/tuempresa/possystem/presentation/reportes/PantallaReportes.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
@@ -102,7 +103,7 @@ fun PantallaReportes(
         }
     }
 
-    Surface(modifier = Modifier.fillMaxSize(), color = FondoCarbon) {
+    Surface(modifier = Modifier.fillMaxSize().imePadding(), color = FondoCarbon) {
         Column(
             modifier = Modifier
                 .fillMaxSize()
