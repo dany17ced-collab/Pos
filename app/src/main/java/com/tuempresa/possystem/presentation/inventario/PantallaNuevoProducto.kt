5a9b6dc091edf7827c4deadc3e391175403c0b3b
dany17ced-collab <dany17ced@gmail.com>
2026-09-24 16:44:49 -0500

    Add files via upload

--- app/src/main/java/com/tuempresa/possystem/presentation/inventario/PantallaNuevoProducto.kt
+++ app/src/main/java/com/tuempresa/possystem/presentation/inventario/PantallaNuevoProducto.kt
@@ -14,7 +14,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -115,7 +114,7 @@ fun PantallaNuevoProducto(app: POSApplication, onVolver: () -> Unit, onGuardado:
         return
     }
 
-    Surface(modifier = Modifier.fillMaxSize().imePadding(), color = FondoCarbon) {
+    Surface(modifier = Modifier.fillMaxSize(), color = FondoCarbon) {
         if (mostrandoEscaner) {
             Box(modifier = Modifier.fillMaxSize()) {
                 EscanerCodigoBarras(
