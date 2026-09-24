400e2930a1e46b878441a7ac480dacd2200a0e1b
dany17ced-collab <dany17ced@gmail.com>
2026-09-24 03:38:24 -0500

    Add files via upload

--- app/src/main/java/com/tuempresa/possystem/presentation/home/PantallaInicioAdmin.kt
+++ app/src/main/java/com/tuempresa/possystem/presentation/home/PantallaInicioAdmin.kt
@@ -228,7 +228,7 @@ fun PantallaInicioAdmin(
             }
 
             Row(
-                modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp, top = 10.dp),
+                modifier = Modifier.fillMaxWidth().padding(start = 16.dp, end = 16.dp, top = 10.dp),
                 horizontalArrangement = Arrangement.spacedBy(10.dp)
             ) {
                 BotonGestion(
